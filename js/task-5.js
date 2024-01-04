43fb17775f97566c4a1a2476a049c1f68cf705c4
Andrey Svyrskyi <akkad1v@gmail.com>
2024-01-04 23:14:06 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,16 @@
+const colorBtnChange = document.querySelector(".change-color");
+const colorSpanChange = document.querySelector(".color");
+const bodyColorChange = document.body;
+
+colorBtnChange.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const newColor = getRandomHexColor();
+
+  bodyColorChange.style.backgroundColor = newColor;
+  colorSpanChange.textContent = newColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
