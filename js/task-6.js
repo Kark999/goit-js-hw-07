bbb1f708bac6352f2783412597d24dcca87c1029
Andrey Svyrskyi <akkad1v@gmail.com>
2024-01-05 04:02:22 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,38 @@
+const input = document.querySelector("#controls input");
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = input.value;
+  if (amount < 1 || amount > 100 || isNaN(amount)) {
+    // alert("Please enter a number between 1 and 100.");
+    return;
+  }
+  boxesContainer.innerHTML = "";
+
+  let boxes = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  boxesContainer.append(...boxes);
+  input.value = "";
+  // console.log(boxesContainer);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
 }
